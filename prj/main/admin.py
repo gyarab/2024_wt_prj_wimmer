4dc0237354ab1c5ec54d4be120ffa7efb5651a9c
sicelet1 <jakub.wimmer7@gmail.com>
2025-03-21 10:23:05 +0100

    some samples for DB

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Movie, Director
 
-# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "year", "director", "description"]
+
+    
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "birth_year", "description"]
+
+#Sample:
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Director, DirectorAdmin)
