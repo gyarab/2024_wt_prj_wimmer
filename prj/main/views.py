b7ef2cab68c837ac6caeb73d64b0515793ae0551
sicelet1 <jakub.wimmer7@gmail.com>
2025-05-02 10:11:33 +0200

    test

--- prj/main/views.py
+++ prj/main/views.py
@@ -6,8 +6,15 @@ def get_homepage(request):
     context = {
 
         "svatek": "Libor",
-        # SELECT * from Products;
-        "movies": Product.objects.all()
+        # SELECT * from Products ORDER BY "title" LIMIT 10;
+        "movies": Product.objects.all().order_by("title")[:10]
     }
+
+    products = Product.objects.all().order_by("title")
+
+    #check if search parameter is in the request
+    if request.GET.get("search"):
+        print("SEARCH", request.GET.get("search"))
+        products = products.filter(title__icontains=request.GET.get("search"))
  
     return render(request, "main/homepage.html", context)
